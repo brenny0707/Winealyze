58ee5fd5eec19dc686f151b34229ee1bfa4d00a5
Brendan Ko <brenny0707@gmail.com>
2017-12-18 16:31:27 -0500

    Add user model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,37 @@ class User < ApplicationRecord
   validates :password, length: {minimum: 6, allow_nil: true}
 
   after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && user.is_password?(password)
+      return user
+    end
+    nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.url_safe64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(@password)
+  end
+
+  # private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
